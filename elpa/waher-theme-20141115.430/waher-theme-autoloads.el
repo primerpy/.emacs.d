2938927f9651d4978c7cd15a0a7aacb9ae4e6d21
PrimerPy <primerpy3@gmail.com>
2017-11-15 18:24:29 -0500

    updated config

---
+++ elpa/waher-theme-20141115.430/waher-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; waher-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "waher-theme" "waher-theme.el" (23051 2979
+;;;;;;  832715 265000))
+;;; Generated autoloads from waher-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; waher-theme-autoloads.el ends here
---
+++ elpa/waher-theme-20141115.430/waher-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "waher-theme" "20141115.430" "Emacs 24 theme based on waher for st2 by dduckster" '((emacs "24.1")) :commit "60d31519fcfd8e797723d47961b255ae2f2e2c0a" :url "https://github.com/jasonm23/emacs-waher-theme")
